f33837b931adc02d6a28af837f7116654288b869
weversonmp <weversonmnzs@gmail.com>
2023-10-02 09:20:13 -0300

    Initial commit

---
+++ aula03/_modelophp.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="_css/estilo.css" />
+  <meta charset="UTF-8" />
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+
+<body>
+  <div>
+    <?php
+    $nome = 'Weverson';
+    $idade = 30;
+    echo "$nome tem $idade anos.";
+
+    ?>
+  </div>
+</body>
+
+</html>
